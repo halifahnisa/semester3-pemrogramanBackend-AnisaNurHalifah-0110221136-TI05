466dab8cef5e6109ad09a219841de7d4455d3f66
halifahnisa <92670576+halifahnisa@users.noreply.github.com>
2023-01-01 19:39:31 +0700

    Add files via upload

---
+++ Pertemuan 13/data/students.js
@@ -0,0 +1,4 @@
+//Buat Data Stidents
+const students = ["Gita", "Wanda", "Amir"];
+//export data students
+module.exports = students;
---
+++ Pertemuan 13/routes/api.js
@@ -0,0 +1,25 @@
+// import StudentController
+const StudentController = require("../controllers/StudentController");
+
+// import express
+const express = require("express");
+
+// membuat object router
+const router = express.Router();
+
+/**
+ * Membuat routing
+ */
+router.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+router.get("/students", StudentController.index);
+router.post("/students", StudentController.store);
+router.put("/students/:id", StudentController.update);
+router.delete("/students/:id", StudentController.destroy);
+//menambahkan route untuk get detail resource
+router.get("/students/:id", StudentController.show);
+
+// export router
+module.exports = router;
